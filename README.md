26f803f51744348ac1225aaf006952229b2ec408
pattywagonpbj <patrick.jarrett11@gmail.com>
2021-01-11 00:45:22 -0800

    update about-me

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # about-me
+
+# LAB -02
+
+## About me
+
+Hello, my name is Patrick, but everyone calls me PJ for short. I spent 8 years in the Marine Corps and I got out in Dec of 2019.
+
+### Author: Student/Group Name
+
+### Links and Resources
+* [submission PR](http://xyz.com)
+* Any Links you used as reference
+
+### Reflections and Comments
+* Consider including the answers to your daily journal and submission questions here
+* This is also a good place to reflect on the tools and resources used and learned
---
+++ js/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//linter test working
+
+var banana = 42
+
+// get user name and offer greeting
+
+var userName = prompt ('Hey, how\'s it going? What \'s your name pal?');
+
+
+var questionOne = prompt('Do I live in Washington').toLowerCase();
+if(questionOne === 'yes'|| questionOne ==='y'){
+  // console.log('you are correct')
+  alert('you are correct'); 
+} else if ( questionOne === 'no' || questionOne === 'n'){
+  alert ('You are so wrong!');
+}
+
+var questionTwo = prompt('Do I like snowboarding?').toLowerCase();
+if(questionTwo === 'yes' || questionTwo === 'y') {
+    // console.log('That is correct!')
+    alert('That\'s also correct');
+} else if ( questionTwo === 'no' || questionTwo === 'n'){
+  alert ('Wrong! I love it!');
+}
+
+var questionThree = prompt('Do I love sushi?').toLowerCase();
+if(questionThree === 'yes'|| questionThree ==='y'){
+  // console.log('you are correct')
+  alert('you are correct'); 
+} else if ( questionThree === 'no' || questionThree === 'n'){
+  alert ('PFFFTTTT that doesn\'t sound like me!');
+}
+
+var questionFour = prompt('Do I like scubadiving?').toLowerCase();
+if(questionFour === 'yes'|| questionFour ==='y'){
+  // console.log('you are correct')
+  alert('you are correct'); 
+} else if ( questionFour === 'no' || questionFour === 'n'){
+  alert ('Wow, you got it.......NOT!');
+}
+
+var questionFive = prompt('Do I put my family first?').toLowerCase();
+if(questionFive === 'yes'|| questionFive ==='y'){
+  // console.log('you are correct')
+  alert('you are correct'); 
+} else if ( questionFive === 'no' || questionFive === 'n'){
+  alert ('You need to rethink that and come back with an absolute YES!');
+}
