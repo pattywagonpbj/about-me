661298d9785511c4dcbd8163208e60d5d45bc74f
pattywagonpbj <patrick.jarrett11@gmail.com>
2021-01-11 03:09:52 -0800

    update

--- js/app.js
+++ js/app.js
@@ -1,58 +1,54 @@
 'use strict';
 
-//linter test working
-
-var banana = 42
-
 // get user name and offer greeting
 
-var userName = prompt ('Hey, how\'s it going? What\'s your name pal?');
-alert('Welcome ' + userName + ' to my website')
+var userName = prompt('Hey, how\'s it going? What\'s your name pal?');
+alert('Welcome ' + userName + ' to my website');
 
 // question one
 var answerOne = prompt('Do I live in Washington').toLowerCase();
-if(answerOne === 'yes'|| answerOne ==='y'){
+if (answerOne === 'yes' || answerOne === 'y') {
   // console.log('you are correct')
-  alert('Spot on!'); 
-} else if ( answerOne === 'no' || answerOne === 'n'){
-  alert ('You are so wrong!');
+  alert('Spot on!');
+} else if (answerOne === 'no' || answerOne === 'n') {
+  alert('You are so wrong!');
 }
 
 // question two
 var answerTwo = prompt('Do I like snowboarding?').toLowerCase();
-if(answerTwo === 'yes' || answerTwo === 'y') {
-    // console.log('That is correct!')
-    alert('That\'s also correct');
-} else if ( answerTwo === 'no' || answerTwo === 'n'){
-  alert ('Wrong! I love it!');
+if (answerTwo === 'yes' || answerTwo === 'y') {
+  // console.log('That is correct!')
+  alert('That\'s also correct');
+} else if (answerTwo === 'no' || answerTwo === 'n') {
+  alert('Wrong! I love it!');
 }
 
 // question three
 var answerThree = prompt('Do I love sushi?').toLowerCase();
-if(answerThree === 'yes'|| answerThree ==='y'){
+if (answerThree === 'yes' || answerThree === 'y') {
   // console.log('you are correct')
-  alert('You\'re on a role!'); 
-} else if ( answerThree === 'no' || answerThree === 'n'){
-  alert ('PFFFTTTT that doesn\'t sound like me!');
+  alert('You\'re on a role!');
+} else if (answerThree === 'no' || answerThree === 'n') {
+  alert('PFFFTTTT that doesn\'t sound like me!');
 }
 
 // question four
 var answerFour = prompt('Do I like scubadiving?').toLowerCase();
-if(answerFour === 'yes'|| answerFour ==='y'){
+if (answerFour === 'yes' || answerFour === 'y') {
   // console.log('you are correct')
-  alert('Another one Correct!'); 
-} else if ( answerFour === 'no' || answerFour === 'n'){
-  alert ('Wow, you got it.......NOT!');
+  alert('Another one Correct!');
+} else if (answerFour === 'no' || answerFour === 'n') {
+  alert('Wow, you got it.......NOT!');
 }
 
 // question five
 var answerFive = prompt('Do I put my family first?').toLowerCase();
-if(answerFive === 'yes'|| answerFive ==='y'){
+if (answerFive === 'yes' || answerFive === 'y') {
   // console.log('you are correct')
-  alert('Amazing!'); 
-} else if ( answerFive === 'no' || answerFive === 'n'){
-  alert ('You need to rethink that and come back with an absolute YES!');
+  alert('Amazing!');
+} else if (answerFive === 'no' || answerFive === 'n') {
+  alert('You need to rethink that and come back with an absolute YES!');
 }
 
 // var userName = prompt ('Well that\'s a little bit about me, I hope you enjoyed my website?');
-alert('Goodbye, ' + userName + ' I hope you enjoyed my website.')
+alert('Goodbye, ' + userName + ' I hope you enjoyed my website.');
