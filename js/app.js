a2a07903b9c517470b184189980537c985e9e6b8
pattywagonpbj <patrick.jarrett11@gmail.com>
2021-01-11 07:20:25 -0800

    update lab

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ alert('Welcome ' + userName + ' to my website');
 // question one
 var answerOne = prompt('Do I live in Washington').toLowerCase();
 if (answerOne === 'yes' || answerOne === 'y') {
-  // console.log('you are correct')
+  // console.log('you are correct');
   alert('Spot on!');
 } else if (answerOne === 'no' || answerOne === 'n') {
   alert('You are so wrong!');
@@ -17,7 +17,7 @@ if (answerOne === 'yes' || answerOne === 'y') {
 // question two
 var answerTwo = prompt('Do I like snowboarding?').toLowerCase();
 if (answerTwo === 'yes' || answerTwo === 'y') {
-  // console.log('That is correct!')
+  // console.log('That is correct!');
   alert('That\'s also correct');
 } else if (answerTwo === 'no' || answerTwo === 'n') {
   alert('Wrong! I love it!');
@@ -26,7 +26,7 @@ if (answerTwo === 'yes' || answerTwo === 'y') {
 // question three
 var answerThree = prompt('Do I love sushi?').toLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
-  // console.log('you are correct')
+  // console.log('you are correct');
   alert('You\'re on a role!');
 } else if (answerThree === 'no' || answerThree === 'n') {
   alert('PFFFTTTT that doesn\'t sound like me!');
@@ -35,7 +35,7 @@ if (answerThree === 'yes' || answerThree === 'y') {
 // question four
 var answerFour = prompt('Do I like scubadiving?').toLowerCase();
 if (answerFour === 'yes' || answerFour === 'y') {
-  // console.log('you are correct')
+  // console.log('you are correct');
   alert('Another one Correct!');
 } else if (answerFour === 'no' || answerFour === 'n') {
   alert('Wow, you got it.......NOT!');
@@ -44,7 +44,7 @@ if (answerFour === 'yes' || answerFour === 'y') {
 // question five
 var answerFive = prompt('Do I put my family first?').toLowerCase();
 if (answerFive === 'yes' || answerFive === 'y') {
-  // console.log('you are correct')
+  // console.log('you are correct');
   alert('Amazing!');
 } else if (answerFive === 'no' || answerFive === 'n') {
   alert('You need to rethink that and come back with an absolute YES!');
